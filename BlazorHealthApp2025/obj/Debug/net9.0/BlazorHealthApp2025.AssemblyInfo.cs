f95312cb4ba0a62d22e001b153ac97b0128647d4
feove <feove31@gmail.com>
2025-04-03 11:17:03 +0100

    Need to fix Schedule frontend

--- BlazorHealthApp2025/obj/Debug/net9.0/BlazorHealthApp2025.AssemblyInfo.cs
+++ BlazorHealthApp2025/obj/Debug/net9.0/BlazorHealthApp2025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorHealthApp2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+521db1d85c844db579a433ac86421d83e3c9fb84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de7a59855c5bd4053bf28df2aed526a32fbb975f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorHealthApp2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorHealthApp2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
