6dac1b6405f6cd15b8ea65394c9aa3536d714b79
unknown <leon.corbin.chatel@gmail.com>
2025-04-07 22:51:29 +0200

    feat(icon&calendar): added icon for calendar and color based on day

--- BlazorHealthApp2025/obj/Debug/net9.0/BlazorHealthApp2025.AssemblyInfo.cs
+++ BlazorHealthApp2025/obj/Debug/net9.0/BlazorHealthApp2025.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorHealthApp2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef8b53eadce3ab202f3b4340329b3e7e40ed5af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40ac88681b1290aafffbf0b93f8495d5078375b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorHealthApp2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorHealthApp2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
